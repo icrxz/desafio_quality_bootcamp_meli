1e18193f7f5201eba3d72ab1e1f124f29267b4c7
icrxz <igor.cruz@mercadolivre.com>
2021-07-15 15:37:28 -0300

    refactor: update Property dto district attributes

--- src/main/java/com/meli/testing/desafioquality/dto/PropertyDTO.java
+++ src/main/java/com/meli/testing/desafioquality/dto/PropertyDTO.java
@@ -1,8 +1,8 @@
 package com.meli.testing.desafioquality.dto;
 
-        import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
+
+import com.meli.testing.desafioquality.dto.mapper.DistrictMapper;
 import com.meli.testing.desafioquality.dto.mapper.RoomMapper;
 import com.meli.testing.desafioquality.entity.Property;
 
@@ -12,22 +12,17 @@ public class PropertyDTO {
 
     private String prop_name;
 
-    private String prop_district;
+    private DistrictDTO prop_district;
 
-    private BigDecimal value_district_m2;
-
-    private List<RoomDTO> rooms = new ArrayList<>();
+    private List<RoomDTO> rooms;
 
     public PropertyDTO(Property property) {
-
         this.id = property.getId();
         this.prop_name = property.getName();
-        this.prop_district = property.getDistrict();
-        this.value_district_m2 = property.getValue_district_m2();
         this.rooms = RoomMapper.convertRoomListToDTO(property.getRooms()) ;
+        this.prop_district = DistrictMapper.convert(property.getDistrict());
     }
-    
-   
+
     public Long getId() {
 		return id;
 	}
@@ -44,20 +39,12 @@ public class PropertyDTO {
 		this.prop_name = prop_name;
 	}
 
-	public String getProp_district() {
+	public DistrictDTO getProp_district_id() {
 		return prop_district;
 	}
 
-	public void setProp_district(String prop_district) {
+	public void setProp_district(DistrictDTO prop_district) {
 		this.prop_district = prop_district;
-	}
-
-	public BigDecimal getValue_district_m2() {
-		return value_district_m2;
-	}
-
-	public void setValue_district_m2(BigDecimal value_district_m2) {
-		this.value_district_m2 = value_district_m2;
 	}
 
 	public List<RoomDTO> getRooms() {
