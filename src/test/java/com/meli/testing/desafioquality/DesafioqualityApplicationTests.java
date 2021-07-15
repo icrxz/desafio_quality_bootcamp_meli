30fee2d57fb530d6db72ff37d739b80b892abf2d
icrxz <igor.cruz@mercadolivre.com>
2021-07-15 09:37:21 -0300

    chore: create project with dependencies

---
+++ src/test/java/com/meli/testing/desafioquality/DesafioqualityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.testing.desafioquality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioqualityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
