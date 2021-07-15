165367332794c90af3e7e8ebf1effaf3f50a9bb4
icrxz <igor.cruz@mercadolivre.com>
2021-07-15 10:44:59 -0300

    chore: add missing @Repository annotations

--- src/main/java/com/meli/testing/desafioquality/repository/RoomRepository.java
+++ src/main/java/com/meli/testing/desafioquality/repository/RoomRepository.java
@@ -2,6 +2,8 @@ package com.meli.testing.desafioquality.repository;
 
 import com.meli.testing.desafioquality.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
 }
