ec448f8c6dbf8378bf6407699db475f461d0dd7b
icrxz <igor.cruz@mercadolivre.com>
2021-07-15 10:45:19 -0300

    chore: create Room service

---
+++ src/main/java/com/meli/testing/desafioquality/service/RoomService.java
@@ -0,0 +1,15 @@
+package com.meli.testing.desafioquality.service;
+
+import com.meli.testing.desafioquality.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoomService {
+    private final RoomRepository roomRepository;
+
+    @Autowired
+    public RoomService(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+}
