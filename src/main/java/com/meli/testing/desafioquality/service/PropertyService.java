6fb56e3641f3a2e86c2029413c9805e76d1fdc23
Waschington Luiz Menezes Filho <waschington.menezes@mercadolivre.com>
2021-07-15 16:12:22 -0300

    feat: implements a method to find a property by id or throws if not found

--- src/main/java/com/meli/testing/desafioquality/service/PropertyService.java
+++ src/main/java/com/meli/testing/desafioquality/service/PropertyService.java
@@ -1,5 +1,7 @@
 package com.meli.testing.desafioquality.service;
 
+import com.meli.testing.desafioquality.entity.Property;
+import com.meli.testing.desafioquality.exception.PropertyNotFoundException;
 import com.meli.testing.desafioquality.repository.PropertyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,5 +13,13 @@ public class PropertyService {
     @Autowired
     public PropertyService(PropertyRepository propertyRepository) {
         this.propertyRepository = propertyRepository;
+    }
+
+    public Property findPropertyById(long id) {
+        Property property = this.propertyRepository.findById(id).stream().findFirst().orElse(null);
+        if (property == null) {
+            throw new PropertyNotFoundException("Propriedade não encontrada");
+        }
+        return property;
     }
 }
