d96b519753734537b2b3e057203fd4233bbd158f
Jonathan <jonathan.cavalcanti@mercadolivre.com>
2021-07-15 12:13:41 -0300

    fix:update RoomDTO

--- src/main/java/com/meli/testing/desafioquality/dto/RoomDTO.java
+++ src/main/java/com/meli/testing/desafioquality/dto/RoomDTO.java
@@ -2,62 +2,49 @@ package com.meli.testing.desafioquality.dto;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
 import com.meli.testing.desafioquality.entity.Room;
 
 public class RoomDTO {
 
-    private Long id;
+	private String room_name;
 
-    private String name;
+	private Double room_width;
 
-    private Double width;
+	private Double room_length;
 
-    private Double length;
+	public RoomDTO(Room room) {
+		this.room_name = room.getName();
+		this.room_width = room.getWidth();
+		this.room_length = room.getLength();
+	}
 
-    public RoomDTO(Room room) {
-        this.id = room.getId();
-        this.name = room.getName();
-        this.width = room.getWidth();
-        this.length = room.getLength();
-    }
+	public String getRoom_name() {
+		return room_name;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public void setRoom_name(String room_name) {
+		this.room_name = room_name;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public Double getRoom_width() {
+		return room_width;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public void setRoom_width(Double room_width) {
+		this.room_width = room_width;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public Double getRoom_length() {
+		return room_length;
+	}
 
-    public Double getWidth() {
-        return width;
-    }
+	public void setRoom_length(Double room_length) {
+		this.room_length = room_length;
+	}
 
-    public void setWidth(Double width) {
-        this.width = width;
-    }
+	public static List<RoomDTO> converter(List<Room> room) {
 
-    public Double getLength() {
-        return length;
-    }
-
-    public void setLength(Double length) {
-        this.length = length;
-    }
-
-    public static List<RoomDTO> converter(List<Room> room) {
-
-        return room.stream().map(RoomDTO::new).collect(Collectors.toList());
-    }
-
+		return room.stream().map(RoomDTO::new).collect(Collectors.toList());
+	}
 
 }
