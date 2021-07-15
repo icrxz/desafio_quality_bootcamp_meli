b45b9eea1ad42850128c2d96a2d195de1aa0f3e3
Jonathan <jonathan.cavalcanti@mercadolivre.com>
2021-07-15 11:17:52 -0300

    fix:update RoomDTO

--- src/main/java/com/meli/testing/desafioquality/dto/RoomDTO.java
+++ src/main/java/com/meli/testing/desafioquality/dto/RoomDTO.java
@@ -1,4 +1,63 @@
 package com.meli.testing.desafioquality.dto;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.meli.testing.desafioquality.entity.Room;
+
 public class RoomDTO {
+
+    private Long id;
+
+    private String name;
+
+    private Double width;
+
+    private Double length;
+
+    public RoomDTO(Room room) {
+        this.id = room.getId();
+        this.name = room.getName();
+        this.width = room.getWidth();
+        this.length = room.getLength();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getWidth() {
+        return width;
+    }
+
+    public void setWidth(Double width) {
+        this.width = width;
+    }
+
+    public Double getLength() {
+        return length;
+    }
+
+    public void setLength(Double length) {
+        this.length = length;
+    }
+
+    public static List<RoomDTO> converter(List<Room> room) {
+
+        return room.stream().map(RoomDTO::new).collect(Collectors.toList());
+    }
+
+
 }
