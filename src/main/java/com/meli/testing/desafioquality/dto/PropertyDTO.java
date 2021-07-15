85e823d7162913dd7bffe12ab80755ff6f38aba1
Jonathan <jonathan.cavalcanti@mercadolivre.com>
2021-07-15 12:13:07 -0300

    fix:update PropertyDTO

--- src/main/java/com/meli/testing/desafioquality/dto/PropertyDTO.java
+++ src/main/java/com/meli/testing/desafioquality/dto/PropertyDTO.java
@@ -4,7 +4,6 @@ package com.meli.testing.desafioquality.dto;
         import java.util.ArrayList;
         import java.util.List;
         import java.util.stream.Collectors;
-
         import com.meli.testing.desafioquality.entity.Property;
         import com.meli.testing.desafioquality.entity.Room;
 
@@ -12,64 +11,70 @@ public class PropertyDTO {
 
     private Long id;
 
-    private String name;
+    private String prop_name;
 
-    private String district;
+    private String prop_district;
 
     private BigDecimal value_district_m2;
 
-    private List<Room> rooms = new ArrayList<>();
+    private List<RoomDTO> rooms = new ArrayList<>();
 
     public PropertyDTO(Property property) {
 
         this.id = property.getId();
-        this.name = property.getName();
-        this.district = property.getDistrict();
+        this.prop_name = property.getName();
+        this.prop_district = property.getDistrict();
         this.value_district_m2 = property.getValue_district_m2();
-        this.rooms = property.getRooms();
+        this.rooms = RoomMapperproperty.getRooms();
     }
-
+    
+   
     public Long getId() {
-        return id;
-    }
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getProp_name() {
+		return prop_name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setProp_name(String prop_name) {
+		this.prop_name = prop_name;
+	}
 
-    public String getDistrict() {
-        return district;
-    }
+	public String getProp_district() {
+		return prop_district;
+	}
 
-    public void setDistrict(String district) {
-        this.district = district;
-    }
+	public void setProp_district(String prop_district) {
+		this.prop_district = prop_district;
+	}
 
-    public BigDecimal getValue_district_m2() {
-        return value_district_m2;
-    }
+	public BigDecimal getValue_district_m2() {
+		return value_district_m2;
+	}
 
-    public void setValue_district_m2(BigDecimal value_district_m2) {
-        this.value_district_m2 = value_district_m2;
-    }
+	public void setValue_district_m2(BigDecimal value_district_m2) {
+		this.value_district_m2 = value_district_m2;
+	}
 
-    public List<Room> getRooms() {
-        return rooms;
-    }
+	public List<RoomDTO> getRooms() {
+		return rooms;
+	}
 
-    public void setRooms(List<Room> rooms) {
-        this.rooms = rooms;
-    }
-
-    public static List<PropertyDTO> converter(List<Property> property) {
+	public void setRooms(List<RoomDTO> rooms) {
+		this.rooms = rooms;
+	}
+	 
+	public static List<RoomDTO> convertRoomListToDTO(List<Room> rooms) {
+	        return rooms.stream().map(RoomDTO::new).collect(Collectors.toList());
+	    }
+	 
+	 
+	public static List<PropertyDTO> converter(List<Property> property) {
 
         return property.stream().map(PropertyDTO::new).collect(Collectors.toList());
     }
