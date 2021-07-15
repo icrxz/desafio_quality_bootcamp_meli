4b643c2588a1b4e0269c13d4ee4514519811a0ad
Waschington Luiz Menezes Filho <waschington.menezes@mercadolivre.com>
2021-07-15 16:13:04 -0300

    refactor: rename util class

--- src/main/java/com/meli/testing/desafioquality/utils/CalculateRoom.java
+++ src/main/java/com/meli/testing/desafioquality/utils/CalculateRoom.java
@@ -2,7 +2,7 @@ package com.meli.testing.desafioquality.utils;
 
 import com.meli.testing.desafioquality.entity.Room;
 
-public class CalculateRooms {
+public class CalculateRoom {
 
     public static Double calculateArea(Room room) {
         return room.getWidth() * room.getLength();
