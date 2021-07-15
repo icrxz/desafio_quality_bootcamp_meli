5707b29934f7a76345cb2c8b95fa9798eb561d84
Jonathan <jonathan.cavalcanti@mercadolivre.com>
2021-07-15 12:33:22 -0300

    fix:update PropertyMapper

--- src/main/java/com/meli/testing/desafioquality/dto/mapper/PropertyMapper.java
+++ src/main/java/com/meli/testing/desafioquality/dto/mapper/PropertyMapper.java
@@ -1,4 +1,14 @@
 package com.meli.testing.desafioquality.dto.mapper;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import com.meli.testing.desafioquality.dto.PropertyDTO;
+import com.meli.testing.desafioquality.entity.Property;
+
 public class PropertyMapper {
+
+	public static List<PropertyDTO> converter(List<Property> property) {
+
+		return property.stream().map(PropertyDTO::new).collect(Collectors.toList());
+	}
 }
