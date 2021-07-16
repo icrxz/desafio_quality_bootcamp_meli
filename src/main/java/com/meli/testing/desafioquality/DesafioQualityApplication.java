0e9886d78be2c1f4d6829a2670d71615ef765616
icrxz <igor.cruz@mercadolivre.com>
2021-07-16 12:06:48 -0300

    fix: fix typo in app name

--- src/main/java/com/meli/testing/desafioquality/DesafioQualityApplication.java
+++ src/main/java/com/meli/testing/desafioquality/DesafioQualityApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DesafioqualityApplication {
+public class DesafioQualityApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DesafioqualityApplication.class, args);
+        SpringApplication.run(DesafioQualityApplication.class, args);
     }
 
 }
