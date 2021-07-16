e25163312aa8337edec16f593e6215b157dce9ec
icrxz <igor.cruz@mercadolivre.com>
2021-07-16 17:09:03 -0300

    refactor: use lambda to create list

--- src/main/java/com/meli/testing/desafioquality/dto/mapper/RoomMapper.java
+++ src/main/java/com/meli/testing/desafioquality/dto/mapper/RoomMapper.java
@@ -1,6 +1,5 @@
 package com.meli.testing.desafioquality.dto.mapper;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import com.meli.testing.desafioquality.dto.room.RoomDTO;
@@ -9,17 +8,14 @@ import com.meli.testing.desafioquality.form.RoomForm;
 
 public class RoomMapper {
 
-	public static List<RoomDTO> convert(List<Room> room) {
-		return room.stream().map(RoomDTO::new).collect(Collectors.toList());
-	}
-
 	public static List<RoomDTO> convertRoomListToDTO(List<Room> rooms) {
 		return rooms.stream().map(RoomDTO::new).collect(Collectors.toList());
 	}
 
 	public static List<Room> convertRoomFormToForm(List<RoomForm> rooms) {
-		List<Room> listRooms = new ArrayList<>();
-		rooms.forEach(r -> listRooms.add(new Room(r.getRoom_name(),r.getRoom_width(), r.getRoom_length())));
-		return listRooms;
+		return rooms
+				.stream()
+				.map(r -> new Room(r.getRoom_name(),r.getRoom_width(), r.getRoom_length()))
+				.collect(Collectors.toList());
 	}
 }
