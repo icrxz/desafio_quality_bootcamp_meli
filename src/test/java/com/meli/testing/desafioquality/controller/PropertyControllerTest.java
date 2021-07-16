6b270bb099eab0e10dd8cb3a26e5cf1775a1692e
Jonathan <jonathan.cavalcanti@mercadolivre.com>
2021-07-16 11:11:58 -0300

    feat: create test Controller

---
+++ src/test/java/com/meli/testing/desafioquality/controller/PropertyControllerTest.java
@@ -0,0 +1,51 @@
+package com.meli.testing.desafioquality.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.meli.testing.desafioquality.service.PropertyService;
+ 
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+class PropertyControllerTest {
+	
+	@Autowired
+	private MockMvc mock;
+	
+	@Autowired
+	private PropertyService propertyService;
+	
+	@BeforeEach
+	public void init() {
+		
+	}
+
+	@Test
+	public void checkinResultPropertyCalculationM2() throws Exception {
+		
+		mock.perform(get("/property/calculate-m2/{id}", 1L))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.prop_m2").value(1590.0));
+		
+	}
+	
+	
+	 
+
+}
